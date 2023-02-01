d9dd3f197c85fa982aef2a7ea1777a6601521990
Cyril JACQUIOD <cyriljacquiod@macbook-air-de-cyril.home>
2023-02-01 13:48:43 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import "./App.css";
-import Section from "./components/Section";
+import data from "./asset/data/data.json";
+import logo from "./asset/img/logo.jpeg";
 
-const tab = [{ category, images }];
-const tab2 = tab.map((elem, index));
-{
-  console.log(tab2);
+// APELLE LA FONCTION APP
+function App() {
+  // console.log(data)POUR VERIFIER;
+
+  // RENVOI PAR CATEGORY LES ELEMENTS DU TABLEAU INCLUS DANS data.json
+  return (
+    // DEBUT DU JSX AVEC LA DIV : LES FONCTIONS OBJETS ET VARIABLES SONT ENTRE {}
+    <div className="App">
+      {/* INTEGRATION DU LOGO NETFLIX DANS LE HEADER */}
+      <header>
+        <img src={logo} alt="" />
+      </header>
+      {/* CONVERTI LE DATA.JSON EN TABLEAU JSX (MAP) AVEC UN ELEMENT (ITEM ET UN INDEX*/}
+      {data.map((item, index) => {
+        // console.log(item);
+
+        return (
+          // UTILISE UNE PROPS DANS LE DOSSEIR COMPONENT
+          // <Section key={index} elem={elem} />
+
+          <section key={index}>
+            <h2>{item.category}</h2>
+            <div>
+              {item.images.map((image, num) => {
+                return <img key={num} src={image} alt="movie" />;
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,12 @@
-import data from "./data.json";
-const Section = () => {
-  return (
-    <div>
-      {data.map((item) => {
-        console.log(item);
-      })}
-    </div>
-  );
-};
+// import data from "./data.json";
+// const Section = () => {
+//   return (
+//     <div>
+//       {data.map((item) => {
+//         console.log(item);
+//       })}
+//     </div>
+//   );
+// };
 
-export default Section;
+// export default Section;
